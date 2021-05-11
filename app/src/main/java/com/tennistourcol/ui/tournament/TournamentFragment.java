48460108e0e2c4c8f66d79f870bc092f77e378f5
juanmd9 <juanmmd9@gmail.com>
2021-05-11 17:24:39 -0500

    Avance en cración de torneo

---
+++ app/src/main/java/com/tennistourcol/ui/tournament/TournamentFragment.java
@@ -0,0 +1,35 @@
+package com.tennistourcol.ui.tournament;
+
+import android.arch.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tennistourcol.R;
+
+public class TournamentFragment extends Fragment {
+
+    private TournamentViewModel mViewModel;
+
+    public static TournamentFragment newInstance() {
+        return new TournamentFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tournament_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(TournamentViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
