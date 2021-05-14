f2d66e5cd4f526e6f39fb1bc9a301c87012ff27c
JuanManuelHerreraMoya <juan.herrera-m@mail.escuelaing.edu.co>
2021-05-13 19:32:15 -0500

    Modificar torneo y Usuario en dashboard

---
+++ app/src/main/java/com/tennistourcol/service/UserService.java
@@ -0,0 +1,4 @@
+package com.tennistourcol.service;
+
+public interface UserService {
+}
---
+++ app/src/main/java/com/tennistourcol/ui/User/UserViewModel.java
@@ -0,0 +1,6 @@
+package com.tennistourcol.ui.User;
+
+import android.arch.lifecycle.ViewModel;
+
+public class UserViewModel extends ViewModel {
+}
